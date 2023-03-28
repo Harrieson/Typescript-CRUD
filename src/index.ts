598f85a181624bab3ed0eee389ebe0a3cc20df96
Harrieson <harriemburu@gmail.com>
2023-03-28 11:36:11 +0300

    Login and register functionalities

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser"
 import compression from "compression"
 import cors from "cors"
 import mongoose from "mongoose"
+import router from './router'
 
 const app = express()
 app.use(cors({
@@ -23,8 +24,9 @@ server.listen(8080, () => {
 })
 
 
-const MONGO_URL = 'mongodb+srv://harrie:Harr1350n@cluster0.6uiej.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URL = 'mongodb+srv://harrie:Harr1350n@cluster0.6uiej.mongodb.net/typescript-API'
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL)
 mongoose.connection.on('error', (error: Error) => console.log(error) )
+app.use('/', router())
